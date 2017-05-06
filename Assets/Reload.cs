e03507c7052e147df8d3bb2f1b987702965c9e86
TagineerDai <2440897102@qq.com>
2017-05-06 20:58:23 +0800

    25 hours.

---
+++ Assets/Reload.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Reload : MonoBehaviour {
+	public GameObject sphere;
+    Vector3 position;
+    void Start()
+    {
+        position = sphere.transform.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.R))
+        {
+            sphere.transform.position = position;
+            sphere.GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, 0.0f);
+            sphere.GetComponent<Rigidbody>().angularVelocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+	}
+}
